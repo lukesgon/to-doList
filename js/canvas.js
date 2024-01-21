06995986f09d2a2e8032c98a049f339e640e52b7
WildBarreto <wildsbsrreto@gmail.com>
2024-01-21 15:42:26 -0300

    ajustes

--- js/canvas.js
+++ js/canvas.js
@@ -3,36 +3,37 @@ const addTask = document.getElementById('add-task');
 const taskList = [];
 
 class Task {
-    
+
     constructor(description) {
         this.taskId = this.idDefiner();
         this.description = description;
-    };
-    
-    
-    idDefiner() {
-        let randomId = Math.floor(Math.random()*9999);
         
+    };
+
+
+    idDefiner() {
+        let randomId = Math.floor(Math.random() * 9999);
+
         const filter = (array, id) => {
             return array.filter(element => element.taskId === id).length;
         };
-        
+
         let counter = filter(taskList, randomId);
-        
-        while(counter > 0) {
+
+        while (counter > 0) {
             counter = filter(taskList, randomId);
-            
-            randomId = Math.floor(Math.random()*1000);
+
+            randomId = Math.floor(Math.random() * 1000);
         };
-        
+
         return randomId;
     };
-    
+
     taskCreator() {
         const articleElement = document.createElement('article');
         articleElement.classList.add('task-article');
         articleElement.id = `id${this.taskId}`; //necessario ter id antes do numero
-        
+
         const descriptionSectionElement = document.createElement('section');
         descriptionSectionElement.classList.add('description-section');
 
@@ -46,26 +47,35 @@ class Task {
         const checkBoxElement = document.createElement('input');
         checkBoxElement.type = 'checkbox';
         checkBoxElement.classList.add('check-box');
-        
+      
         const descriptionElement = document.createElement('p');
         descriptionElement.textContent = this.description;
         descriptionElement.classList.add('description');
-        
+
+       // Adiciona ouvinte de evento de mudança na checkbox
+       checkBoxElement.addEventListener('change', () => {
+        if (checkBoxElement.checked) {
+            descriptionElement.classList.add('descriptionCheked');
+        } else {
+            descriptionElement.classList.remove('descriptionCheked');
+        }
+    });
+
         const trashBtnElement = document.createElement('button');
         trashBtnElement.classList.add('trash-button');
-        trashBtnElement.addEventListener('click', (e) => {this.taskDelete()})
-        
+        trashBtnElement.addEventListener('click', (e) => { this.taskDelete() })
+
         const trashImgElement = document.createElement('img');
         trashImgElement.src = './assets/trash.svg';
         trashImgElement.alt = "Trash Icon";
         trashImgElement.classList = 'lixeira';
-        
+
         trashBtnElement.appendChild(trashImgElement);
 
         descriptionSectionElement.appendChild(idElement);
         descriptionSectionElement.appendChild(checkBoxElement);
         descriptionSectionElement.appendChild(descriptionElement);
-        
+
         optionsSectionElement.appendChild(trashBtnElement);
 
         articleElement.appendChild(descriptionSectionElement);
@@ -74,21 +84,22 @@ class Task {
         return articleElement;
     };
 
+
     // deleta o elemento HTML/Objeto do tasklist
-    taskDelete(){
+    taskDelete() {
         document.querySelector(`#id${this.taskId}`).remove()
-        taskList.forEach((item, index, object) =>  {
+        taskList.forEach((item, index, object) => {
             if (item.taskId == this.taskId) {
-              object.splice(index, 1);
+                object.splice(index, 1);
             }
-          });
+        });
     }
 
 };
 
 addTask.addEventListener('click', (event) => {
     event.preventDefault();
-    
+
     const descriptionInput = document.getElementById('description');
 
     taskList.push(new Task(descriptionInput.value));
